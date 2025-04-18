f19151ba7f9719cc8c6cd0295ed3feee25696ea2
Sara han <hans63237@gmail.com>
2025-04-18 17:20:19 +0900

    feat: 홈배너작업완료

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Banner from './components/Banner/Banner'
 
+// 1.배너 =>popular movie를 들고와서 첫번째 아이템 보여주기
+// 2.popular movie
+// 3.rop rated movie
+// 4.upcoming movie
 const HomePage = () => {
   return (
     <div>
-      HomePage
+      <Banner/>
     </div>
   )
 }
